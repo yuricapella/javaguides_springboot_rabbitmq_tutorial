38632cc339a513deb62db41f1ad9e654209e2adb
yuricapella <yuricapelladossantos@gmail.com>
2025-07-29 16:47:20 -0300

    feat(producer): adiciona producer para envio de mensagens ao RabbitMQ
    - Implementa classe RabbitMQProducer para publicar mensagens via RabbitTemplate
    - Injeta exchange e routingKey via propriedades externas no application.properties
    - Adiciona logging estruturado para rastrear mensagens enviadas

---
+++ src/main/java/br/com/javaguides/springboot_rabbitmq_tutorial/publisher/RabbitMQProducer.java
@@ -0,0 +1,30 @@
+package br.com.javaguides.springboot_rabbitmq_tutorial.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        LOGGER.info("Message sent -> {}", message);
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
