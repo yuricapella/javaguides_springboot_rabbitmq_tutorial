96bb78dbd0f6c663d0d3e3c76ddd0a27600bed72
yuricapella <yuricapelladossantos@gmail.com>
2025-07-29 16:42:03 -0300

    feat(config): implementa configuração do RabbitMQ com queue, exchange e binding parametrizados
    - Cria classe RabbitMQConfig com beans para Queue, TopicExchange e Binding conforme propriedades externas
    - Injeta parâmetros de queue, exchange e routingKey utilizando @Value para facilitar customização via application.properties
    - Estabelece binding entre queue e exchange utilizando routingKey definida externamente

---
+++ src/main/java/br/com/javaguides/springboot_rabbitmq_tutorial/config/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package br.com.javaguides.springboot_rabbitmq_tutorial.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    //binding entre queue e exchange usando routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+    //Spring boot configura automaticamente essas 3 infraestruturas abaixo:
+    // ConnectionFactory
+    // RabbitTemplate
+    // RabbitAdmin
+}
