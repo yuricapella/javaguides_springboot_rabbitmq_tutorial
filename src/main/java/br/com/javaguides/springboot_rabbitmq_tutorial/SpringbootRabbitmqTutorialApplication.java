ae1b3ebe51b599bcb828e17ff8d3291a7d2fabc9
yuricapella <yuricapelladossantos@gmail.com>
2025-07-29 16:36:03 -0300

    feat: adiciona primeiro commit com application.properties configurado e base do projeto.

---
+++ src/main/java/br/com/javaguides/springboot_rabbitmq_tutorial/SpringbootRabbitmqTutorialApplication.java
@@ -0,0 +1,13 @@
+package br.com.javaguides.springboot_rabbitmq_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javaguides/springboot_rabbitmq_tutorial/SpringbootRabbitmqTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javaguides.springboot_rabbitmq_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
