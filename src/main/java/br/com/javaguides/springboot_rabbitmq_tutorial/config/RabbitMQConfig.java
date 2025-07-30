d4830b46f864976204016eaf46b02e19a84a6cc4
yuricapella <yuricapelladossantos@gmail.com>
2025-07-30 09:48:35 -0300

    feat(config): adiciona suporte a produção de mensagens em JSON no RabbitMQConfig
    - Adiciona atributos para nome de fila e routing key específicas para JSON via propriedades externas
    - Cria beans Queue e Binding para jsonQueue com routingJsonKey
    - Implementa beans MessageConverter e AmqpTemplate configurando Jackson2JsonMessageConverter para serialização JSON nas mensagens
    - adiciona novos atributos globais no application.properties

--- src/main/java/br/com/javaguides/springboot_rabbitmq_tutorial/config/RabbitMQConfig.java
+++ src/main/java/br/com/javaguides/springboot_rabbitmq_tutorial/config/RabbitMQConfig.java
@@ -1,9 +1,10 @@
 package br.com.javaguides.springboot_rabbitmq_tutorial.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +15,17 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.queue.name}")
     private String queue;
 
+    @Value("${rabbitmq.queue.json.name}")
+    private String jsonQueue;
+
     @Value("${rabbitmq.exchange.name}")
     private String exchange;
 
     @Value("${rabbitmq.routing.key}")
     private String routingKey;
+
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
 
     @Bean
     public Queue queue() {
@@ -37,6 +44,30 @@ public class RabbitMQConfig {
                 .bind(queue())
                 .to(exchange())
                 .with(routingKey);
+    }
+
+    // queue (store json)
+    @Bean
+    public Queue jsonQueue(){
+        return new Queue(jsonQueue);
+    }
+
+    @Bean
+    public Binding jsonBinding(){
+        return BindingBuilder
+                .bind(jsonQueue())
+                .to(exchange())
+                .with(routingJsonKey);
+    }
+
+    public MessageConverter converter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
     }
 
     //Spring boot configura automaticamente essas 3 infraestruturas abaixo:
